ce720ea2faaa09fdd8fb6c5a23f89ab2860776ad
JuamMM <40521012+JuamMM@users.noreply.github.com>
2018-11-07 12:40:33 +0100

    Add files via upload

---
+++ Qytetet_Ruby/pkg/Qytetet_Personal-0.0.1/lib/estado_juego.rb
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+module EstadoJuego
+    JA_CONSORPRESA= :con_sorpresa
+    ALGUNJUGADORENBANCARROTA= :en_bancarrota
+    JA_PUEDECOMPRARGESTIONAR= :comprar_gestionar
+    JA_PUEDEGESTIONAR= :puede_gestionar
+    JA_PREPARADO= :preparado
+    JA_ENCARCELADO= :encarcelado
+    JA_ENCARCELADOCONOPCIONDELIBERTAD= :encarcelado_con_opcion_libertad
+end
---
+++ Qytetet_Ruby/pkg/Qytetet_Personal-0.0.1/lib/tipo_casilla.rb
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+module TipoCasilla
+    SALIDA= :salida
+    CARCEL= :carcel
+    JUEZ= :juez
+    PARKING= :parking
+    SORPRESA= :sorpresa
+    CALLE= :calle
+    IMPUESTO= :impuesto
+end
